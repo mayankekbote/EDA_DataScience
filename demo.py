c1ddeb4cbcd00d7a7f84cf1ddc8632ee819df5b2
Mayank <mayankekbote12@gmail.com>
2025-09-01 02:54:45 +0530

    initial

--- demo.py
+++ demo.py
@@ -10,10 +10,10 @@ uploaded_file = st.sidebar.file_uploader("Upload your CSV file", type=["csv"])
 if uploaded_file:
     st.sidebar.success("file loaded succesfully!")
     df=pd.read_csv(uploaded_file)
-    n=st.text_input("Number of Records")
+    n=st.text_input("Number of Records to view")
+    
     click=st.button("View CSV")
     if click:
-        with st.spinner(text="Loading..", show_time=False, width="content"):
-            time.sleep(2.5)
         if n.isnumeric():
             st.dataframe(df.head(int(n)))
+        
